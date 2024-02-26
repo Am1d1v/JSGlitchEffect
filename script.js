83ac809b6cd211dda454035f74974ba8033a6c4d
Am1d1v <dimadiman1adima@gmail.com>
2024-02-26 13:41:26 +0300

    Random glitch appear

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+const container = document.querySelector('.container');
+
+// Create 20 divs for glitch effect
+for(let i = 0; i < 20; i++){
+    const glitchBox =  document.createElement('div');
+    glitchBox.classList.add('box');
+
+    container.appendChild(glitchBox);
+}
+
+const glitches = document.querySelectorAll('.box')
+
+// Random glitch appear
+glitches.forEach((glitch) => {
+    glitch.style.left = Math.random() * 100 + 'vw';
+    glitch.style.top = Math.random() * 100 + 'vh';
+    glitch.style.height = Math.random() * 100 + 'px';
+    glitch.style.width = Math.random() * 400 + 'px';
+});
